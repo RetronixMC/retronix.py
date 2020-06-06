af5bc3ce0939bbdc03fb23373a01a38585703e5f
Ben Foster <benjaminfoster@BEN-PC-01.foster.family>
2020-06-06 19:58:09 +0100

    Really stupid smeg

--- setup.py
+++ setup.py
@@ -1,21 +1,31 @@
+#!/usr/bin/env python
+
 import setuptools as st
-with open("README.md", "r") as f:
-    ld = f.read()
 import retronix as r
-st.setup(
-    name="retronix.py-benfosterdev",
-    version=r.__version__,
-    author="Ben Foster",
-    author_email="ben@benfoster.dev",
-    description="A Python library for the interaction with the Retronix public API",
-    long_description=ld,
-    long_description_content_type="text/markdown",
-    url="https://github.com/retronixmc/retronix.py",
-    packages=st.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independant"
-    ],
-    python_requires='>=3.7'
-)
+
+if __name__ == "__main__":
+
+    with open("README.md", "r") as f:
+        ld = f.read()
+
+    st.setup(
+        name="retronix.py-benfosterdev",
+        version=r.__version__,
+        author="Ben Foster",
+        author_email="ben@benfoster.dev",
+
+        description="A Python library for the interaction with the Retronix public API",
+        long_description=ld,
+        long_description_content_type="text/markdown",
+
+        url="https://github.com/retronixmc/retronix.py",
+
+        packages=st.find_packages(),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independant"
+        ],
+        
+        python_requires='>=3.7'
+    )
